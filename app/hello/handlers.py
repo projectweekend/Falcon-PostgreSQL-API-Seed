aea3e6b01fbf9cfe422dd2af86a5f53317ead710
Brian Hines <brian@projectweekend.net>
2015-04-03 09:09:18 -0500

    Falcon does this for us

--- app/hello/handlers.py
+++ app/hello/handlers.py
@@ -4,7 +4,6 @@ import json
 class HelloResource(object):
 
     def on_get(self, req, resp):
-        resp.content_type = "application/json"
         resp.body = json.dumps({
             "msg": "Hello!",
         })
