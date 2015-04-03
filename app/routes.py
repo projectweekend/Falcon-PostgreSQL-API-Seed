40aa6a799572344e9900ac47054a8a83a1904f98
Brian Hines <brian@projectweekend.net>
2015-04-03 14:52:32 -0500

    Setup routes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from app import api
+from app import api, db
 from hello import handlers as hello_handlers
+from user import handlers as user_handlers
 
 
 api.add_route("/", hello_handlers.HelloResource())
+api.add_route('/user', user_handlers.UserResource(db))
