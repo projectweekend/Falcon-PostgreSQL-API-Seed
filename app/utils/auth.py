42dd27eeb5c68f543e2f50c13fb071183570f0f9
Brian Hines <brian@projectweekend.net>
2015-04-03 15:22:06 -0500

    Encoding

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -2,8 +2,8 @@ import bcrypt
 
 
 def hash_password(password):
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def verify_password(password, hashed):
-    return bcrypt.hashpw(password, hashed) == hashed
+    return bcrypt.hashpw(password.encode('utf-8'), hashed) == hashed
