a35495b81803064be0f71ab154c4d25622634530
Brian Hines <brian@projectweekend.net>
2015-04-03 15:12:25 -0500

    Validate user create request

---
+++ app/user/validation.py
@@ -0,0 +1,21 @@
+import falcon
+from cerberus import Validator
+
+
+def validate_user_create(req, res, resource, params):
+    schema = {
+        'email': {
+            'type': 'string',
+            'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+            'required': True
+        },
+        'password': {
+            'type': 'string',
+            'minlength': 8,
+            'required': True
+        }
+    }
+
+    v = Validator(schema)
+    if not v.validate(req.context['data']):
+        raise falcon.HTTPBadRequest('Bad request', v.errors)
