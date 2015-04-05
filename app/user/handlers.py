cb2ad321a67812e8c514c1f3b311dd2ad9327f51
Brian Hines <brian@projectweekend.net>
2015-04-04 20:22:42 -0500

    Registration handler

--- app/user/handlers.py
+++ app/user/handlers.py
@@ -1,6 +1,8 @@
 import json
 import falcon
-from app.utils.auth import hash_password, verify_password
+from psycopg2 import IntegrityError
+from app import db
+from app.utils.auth import hash_password, generate_token
 from validation import validate_user_create
 
 USER_FIELDS = ['id', 'email']
@@ -8,17 +10,24 @@ USER_FIELDS = ['id', 'email']
 
 class UserResource(object):
 
-    def __init__(self, db):
-        self.db = db
-
     @falcon.before(validate_user_create)
     def on_post(self, req, res):
         email = req.context['data']['email']
         password = hash_password(req.context['data']['password'])
 
-        cursor = self.db.cursor()
-        cursor.callproc('sp_app_user_insert', [email, password])
+        cursor = db.cursor()
+
+        try:
+            cursor.callproc('sp_app_user_insert', [email, password])
+        except IntegrityError:
+            db.rollback()
+            title = 'Conflict'
+            description = 'Email in use'
+            raise falcon.HTTPConflict(title, description)
 
         user_dict = dict(zip(USER_FIELDS, cursor.fetchone()))
+        db.commit()
 
-        res.body = json.dumps(user_dict)
+        res.body = json.dumps({
+            'token': generate_token(user_dict)
+        })
