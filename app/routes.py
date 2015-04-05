9e2e58a937b704a3062d6d670279f013b9fdc502
Brian Hines <brian@projectweekend.net>
2015-04-04 19:38:03 -0500

    Move db elsewhere

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from app import api, db
+from app import api
 from hello import handlers as hello_handlers
 from user import handlers as user_handlers
 
 
 api.add_route("/", hello_handlers.HelloResource())
-api.add_route('/user', user_handlers.UserResource(db))
+api.add_route('/user', user_handlers.UserResource())
