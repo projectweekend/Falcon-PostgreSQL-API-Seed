c9e453357eda573b9480f499a540c511e8ce0b66
Brian Hines <brian@projectweekend.net>
2015-04-04 19:38:38 -0500

    Auth utils

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,4 +1,12 @@
+import os
 import bcrypt
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired, BadSignature
+
+
+TWO_WEEKS = 1209600
+SECRET_KEY = os.getenv('SECRET_KEY', None)
+assert SECRET_KEY
 
 
 def hash_password(password):
@@ -7,3 +15,17 @@ def hash_password(password):
 
 def verify_password(password, hashed):
     return bcrypt.hashpw(password.encode('utf-8'), hashed) == hashed
+
+
+def generate_token(user_dict, expiration=TWO_WEEKS):
+    s = Serializer(SECRET_KEY, expires_in=expiration)
+    return s.dumps(user_dict)
+
+
+def verify_token(token):
+    s = Serializer(SECRET_KEY)
+    try:
+        data = s.loads(token)
+    except (SignatureExpired, BadSignature):
+        return None
+    return data
