5c343f4fd5c5fc2a94ce6939b53f07f96ba99017
Brian Hines <brian@projectweekend.net>
2015-04-06 15:46:39 -0500

    Remove hello world stuff

--- app/hello/handlers.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-class HelloResource(object):
-
-    def on_get(self, req, resp):
-        resp.body = json.dumps({
-            "msg": "Hello!",
-        })
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,6 @@
 from app import api
-from hello import handlers as hello_handlers
 from user import handlers as user_handlers
 
 
-api.add_route('/', hello_handlers.HelloResource())
 api.add_route('/authenticate', user_handlers.AuthenticationResource())
 api.add_route('/user', user_handlers.UserResource())
