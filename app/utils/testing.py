9c79e55d6cfd8698a896c0ca40403813410912d5
Brian Hines <brian@projectweekend.net>
2015-04-06 11:35:43 -0500

    json.loads for my pleasure

--- app/utils/testing.py
+++ app/utils/testing.py
@@ -38,40 +38,45 @@ class APITestCase(TestBase):
 
     def simulate_get(self, path, data):
         self.api = api
-        return self.simulate_request(
+        result = self.simulate_request(
             path=path,
             method='GET',
             headers=HEADERS,
             body=json.dumps(data))
+        return json.loads(result[0])
 
     def simulate_post(self, path, data):
         self.api = api
-        return self.simulate_request(
+        result = self.simulate_request(
             path=path,
             method='POST',
             headers=HEADERS,
             body=json.dumps(data))
+        return json.loads(result[0])
 
     def simulate_put(self, path, data):
         self.api = api
-        return self.simulate_request(
+        result = self.simulate_request(
             path=path,
             method='PUT',
             headers=HEADERS,
             body=json.dumps(data))
+        return json.loads(result[0])
 
     def simulate_patch(self, path, data):
         self.api = api
-        return self.simulate_request(
+        result = self.simulate_request(
             path=path,
             method='PATCH',
             headers=HEADERS,
             body=json.dumps(data))
+        return json.loads(result[0])
 
     def simulate_delete(self, path, data):
         self.api = api
-        return self.simulate_request(
+        result = self.simulate_request(
             path=path,
             method='DELETE',
             headers=HEADERS,
             body=json.dumps(data))
+        return json.loads(result[0])
