8a7e088e64daf2cf5e8f269bc8168d96142010fd
Brian Hines <brian@projectweekend.net>
2015-04-06 11:26:58 -0500

    Set up and tear down migrations

--- app/utils/testing.py
+++ app/utils/testing.py
@@ -1,12 +1,40 @@
 import json
+import subprocess
+from urlparse import urlparse
 from falcon.testing import TestBase
 from app import api
+from app.utils.database import database_url
 
 
 HEADERS = {'Content-Type': 'application/json'}
 
 
 class APITestCase(TestBase):
+
+    def setUp(self):
+        super(APITestCase, self).setUp()
+        self.flyway_migrate()
+
+    def tearDown(self):
+        super(APITestCase, self).tearDown()
+        self.flyway_clean()
+
+    def _run_flyway_command(self, flyway_command):
+        parsed = urlparse(database_url())
+        user = parsed.username
+        host = parsed.hostname
+        port = parsed.port
+        database = parsed.path.strip('/')
+
+        command = 'flyway -url=jdbc:postgresql://{0}:{1}/{2} -user={3} {4}'
+        command = command.format(host, port, database, user, flyway_command)
+        subprocess.check_call(command.split())
+
+    def flyway_migrate(self):
+        self._run_flyway_command('migrate')
+
+    def flyway_clean(self):
+        self._run_flyway_command('clean')
 
     def simulate_get(self, path, data):
         self.api = api
