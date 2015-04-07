6fa74248e47be84ab3b9fa7f5da04e1c84520f9b
Brian Hines <brian@projectweekend.net>
2015-04-07 05:51:21 -0500

    Test database util

---
+++ app/utils/tests.py
@@ -0,0 +1,12 @@
+import os
+from falcon.testing import TestBase
+from database import database_url
+
+
+class DatabaseUtilsTestCase(TestBase):
+
+    def test_database_url(self):
+        del os.environ['DB_PORT_5432_TCP_ADDR']
+        os.environ['DATABASE_URL'] = 'testing'
+        result = database_url()
+        self.assertEqual(result, 'testing')
