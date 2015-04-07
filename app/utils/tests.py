368fc7e51c47bcdbea03a359c6adbaf8d6a3764b
Brian Hines <brian@projectweekend.net>
2015-04-07 10:11:48 -0500

    This only works in docker-compose

--- app/utils/tests.py
+++ app/utils/tests.py
@@ -6,7 +6,8 @@ from database import database_url
 class DatabaseUtilsTestCase(TestBase):
 
     def test_database_url(self):
-        del os.environ['DB_PORT_5432_TCP_ADDR']
+        if os.getenv('DB_PORT_5432_TCP_ADDR', None):
+            del os.environ['DB_PORT_5432_TCP_ADDR']
         os.environ['DATABASE_URL'] = 'testing'
         result = database_url()
         self.assertEqual(result, 'testing')
