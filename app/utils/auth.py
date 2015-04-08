ba730fd895154cc18b3ac8b906a9ef3d0128ea05
Brian Hines <brian@projectweekend.net>
2015-04-08 05:49:31 -0500

    Auth route utils

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -12,6 +12,10 @@ def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def verify_password(password, hashed):
+    return bcrypt.hashpw(password.encode('utf-8'), hashed) == hashed
+
+
 def generate_token(user_dict, expiration=TWO_WEEKS):
     s = Serializer(SECRET_KEY, expires_in=expiration)
     return s.dumps(user_dict)
