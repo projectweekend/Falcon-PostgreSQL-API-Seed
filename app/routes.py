29e5316aad0f43dad51708ede88063f09fdd5247
Brian Hines <brian@projectweekend.net>
2015-04-14 05:37:37 -0500

    Version routes

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,5 @@ from app import api
 from user import handlers as user_handlers
 
 
-api.add_route('/user', user_handlers.UserResource())
-api.add_route('/authenticate', user_handlers.AuthenticationResource())
+api.add_route('/v1/user', user_handlers.UserResource())
+api.add_route('/v1/authenticate', user_handlers.AuthenticationResource())
