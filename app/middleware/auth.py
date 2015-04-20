1851b269405e7d7c21359808a225c50ff1879c81
Brian Hines <brian@projectweekend.net>
2015-04-20 13:24:40 -0500

    Refactor auth middleware

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -4,5 +4,4 @@ from app.utils.auth import verify_token
 class AuthUser(object):
 
     def process_request(self, req, res):
-        if req.auth:
-            req.context['auth_user'] = verify_token(req.auth)
+        req.context['auth_user'] = verify_token(req.auth) if req.auth else None
